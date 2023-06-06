6b56ed4b2a3295cfab8a7deca9c6ec11ff5770fe
Ryuichi Ueda <ryuichiueda@gmail.com>
2023-06-06 01:32:19 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'simple_io2'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Ryuichi Ueda',
+    maintainer_email='ryuichiueda@gmail.com',
+    description='simple_io2: io sample on ROS2',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'pub = simple_io2.pub:main',
+        ],
+    },
+)
---
+++ simple_io2/pub.py
@@ -0,0 +1,25 @@
+import rclpy, random
+from rclpy.node import Node
+from std_msgs.msg import Float64
+
+class Talker(Node):
+    def __init__(self):
+        super().__init__("pub")
+        self.pub = self.create_publisher(Float64, "uniform_rand", 10)
+        self.create_timer(1, self.cb)
+
+
+    def cb(self):
+        msg = Float64()
+        msg.data = random.uniform(0.0, 1.0)
+        self.pub.publish(msg)
+
+
+def main():
+    rclpy.init()
+    talker = Talker()
+    rclpy.spin(talker)
+
+
+if __name__ == "__main__":
+    main()
